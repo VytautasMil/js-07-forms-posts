3a326ee684c468af6e0340f6079ea5b1502d8cec
Vytautas <vyckelis@gmail.com>
2022-07-15 15:29:32 +0300

    locaStorage

--- js/posts.js
+++ js/posts.js
@@ -9,6 +9,14 @@ const authorEl = document.getElementById('author');
 const bodyEl = document.getElementById('body');
 const postsContainerEl = document.querySelector('.posts-container');
 
+const postsArrJsonVersion = localStorage.getItem('postsArr');
+console.log('postsArrJsonVersion ===', postsArrJsonVersion);
+// JSON.parse() - pavercia is Json i javascript
+// jei localStorage nera issaugota arba istrinta postsArr tada nustatom postsArr = []
+const postsArr = postsArrJsonVersion === null ? [] : JSON.parse(postsArrJsonVersion);
+
+render();
+
 createFormEl.addEventListener('submit', function (event) {
   event.preventDefault();
 
@@ -21,7 +29,9 @@ createFormEl.addEventListener('submit', function (event) {
     body: bodyEl.value,
   };
   console.log('newPostObj ===', newPostObj);
-  makePostToHtml(newPostObj, postsContainerEl);
+  // makePostToHtml(newPostObj, postsContainerEl);
+  postsArr.push(newPostObj);
+  render();
   createFormEl.reset();
 });
 
@@ -29,7 +39,7 @@ function makePostToHtml(post, dest) {
   const artEl = document.createElement('div');
   artEl.className = 'post card';
   artEl.innerHTML = `
-    <img src="${post.image}" alt="paveikslelis">
+    <img src="${post.image}" alt="${post.title}">
     <h3 class="post__title">${post.title}</h3>
     <h4 class="post__author">Autorius: ${post.author}</h4>
     <p class="post__date">${post.date}</p>
@@ -38,4 +48,18 @@ function makePostToHtml(post, dest) {
   dest.append(artEl);
 }
 
-const postInnerEls = [['img', '', [['src', '']]]];
+function render() {
+  // isvalyti containeri
+  postsContainerEl.innerHTML = '';
+  // irasyti i localStorage
+  localStorage.setItem('postsArr', JSON.stringify(postsArr));
+  for (let i = 0; i < postsArr.length; i++) {
+    makePostToHtml(postsArr[i], postsContainerEl);
+  }
+}
+
+// loclasstorage
+// localStorage.setItem('postsArr', 'something something');
+
+// const postsArrFromLocalStorage = localStorage.getItem('postsArr');
+// console.log('postsArrFromLocalStorage ===', postsArrFromLocalStorage);
